c8904e0c037c22163bf2bedb2decff983fb9003e
Heiher <r@hev.cc>
2018-10-13 13:46:42 +0800

    HevRInputProtocol: Init.

---
+++ src/hev-rinput-protocol.c
@@ -0,0 +1,11 @@
+/*
+ ============================================================================
+ Name        : hev-rinput-protocol.h
+ Author      : Heiher <r@hev.cc>
+ Copyright   : Copyright (c) 2018 everyone.
+ Description : RInput protocol
+ ============================================================================
+ */
+
+#include "hev-rinput-protocol.h"
+
---
+++ src/hev-rinput-protocol.h
@@ -0,0 +1,23 @@
+/*
+ ============================================================================
+ Name        : hev-rinput-protocol.h
+ Author      : Heiher <r@hev.cc>
+ Copyright   : Copyright (c) 2018 everyone.
+ Description : RInput protocol
+ ============================================================================
+ */
+
+#ifndef __HEV_RINPUT_PROTOCOL_H__
+#define __HEV_RINPUT_PROTOCOL_H__
+
+typedef struct _HevRInputEvent HevRInputEvent;
+
+struct _HevRInputEvent
+{
+	unsigned short type;
+	unsigned short code;
+	unsigned int value;
+} __attribute__((packed));
+
+#endif /* __HEV_RINPUT_PROTOCOL_H__ */
+
