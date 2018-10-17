2cf96a1249a5981facfcdad08875051bf0474c32
Heiher <r@hev.cc>
2018-10-17 21:57:29 +0800

    HevConfig: Sync to new coding style.

--- src/hev-config.c
+++ src/hev-config.c
@@ -21,41 +21,39 @@ static int switch_keycode;
 int
 hev_config_init (const char *config_path)
 {
-	dictionary *ini_dict;
+    dictionary *ini_dict;
 
-	ini_dict = iniparser_load (config_path);
-	if (!ini_dict) {
-		fprintf (stderr, "Load config from file %s failed!\n",
-					config_path);
-		return -1;
-	}
+    ini_dict = iniparser_load (config_path);
+    if (!ini_dict) {
+        fprintf (stderr, "Load config from file %s failed!\n", config_path);
+        return -1;
+    }
 
-	/* Main:Address */
-	char *addr = iniparser_getstring (ini_dict, "Main:Address", NULL);
-	if (!addr) {
-		fprintf (stderr, "Get Main:Address from file %s failed!\n",
-					config_path);
-		iniparser_freedict (ini_dict);
-		return -2;
-	}
-	strncpy (address, addr, 16 - 1);
+    /* Main:Address */
+    char *addr = iniparser_getstring (ini_dict, "Main:Address", NULL);
+    if (!addr) {
+        fprintf (stderr, "Get Main:Address from file %s failed!\n",
+                 config_path);
+        iniparser_freedict (ini_dict);
+        return -2;
+    }
+    strncpy (address, addr, 16 - 1);
 
-	/* Main:Port */
-	int value = iniparser_getint (ini_dict, "Main:Port", -1);
-	if (-1 == value) {
-		fprintf (stderr, "Get Main:Port from file %s failed!\n",
-					config_path);
-		iniparser_freedict (ini_dict);
-		return -3;
-	}
-	port = value;
+    /* Main:Port */
+    int value = iniparser_getint (ini_dict, "Main:Port", -1);
+    if (-1 == value) {
+        fprintf (stderr, "Get Main:Port from file %s failed!\n", config_path);
+        iniparser_freedict (ini_dict);
+        return -3;
+    }
+    port = value;
 
-	/* Main:SwitchKeyCode */
-	switch_keycode = iniparser_getint (ini_dict, "Main:SwitchKeyCode", 0);
+    /* Main:SwitchKeyCode */
+    switch_keycode = iniparser_getint (ini_dict, "Main:SwitchKeyCode", 0);
 
-	iniparser_freedict (ini_dict);
+    iniparser_freedict (ini_dict);
 
-	return 0;
+    return 0;
 }
 
 void
@@ -66,18 +64,17 @@ hev_config_fini (void)
 const char *
 hev_config_get_address (void)
 {
-	return address;
+    return address;
 }
 
 unsigned short
 hev_config_get_port (void)
 {
-	return port;
+    return port;
 }
 
 int
 hev_config_get_rinput_switch_keycode (void)
 {
-	return switch_keycode;
+    return switch_keycode;
 }
-
--- src/hev-config.h
+++ src/hev-config.h
@@ -10,17 +10,16 @@
 #ifndef __HEV_CONFIG_H__
 #define __HEV_CONFIG_H__
 
-#define MAJOR_VERSION		(1)
-#define MINOR_VERSION		(0)
-#define MICRO_VERSION		(0)
+#define MAJOR_VERSION (1)
+#define MINOR_VERSION (0)
+#define MICRO_VERSION (0)
 
 int hev_config_init (const char *config_path);
 void hev_config_fini (void);
 
-const char * hev_config_get_address (void);
+const char *hev_config_get_address (void);
 unsigned short hev_config_get_port (void);
 
 int hev_config_get_rinput_switch_keycode (void);
 
 #endif /* __HEV_CONFIG_H__ */
-
