7f0dcf571a2dcca03d8ded1fe62f0459681bda9f
Heiher <r@hev.cc>
2018-11-07 00:01:09 +0800

    HevConfig: Bump up to 1.2.0.

--- src/hev-config.h
+++ src/hev-config.h
@@ -11,8 +11,8 @@
 #define __HEV_CONFIG_H__
 
 #define MAJOR_VERSION (1)
-#define MINOR_VERSION (1)
-#define MICRO_VERSION (1)
+#define MINOR_VERSION (2)
+#define MICRO_VERSION (0)
 
 int hev_config_init (const char *config_path);
 void hev_config_fini (void);
