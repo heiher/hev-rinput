4b95b34ba247a8d5afe841dd92349640abceadb3
Heiher <r@hev.cc>
2020-06-01 09:31:52 +0800

    HevConfig: Bump up to 1.6.0.

--- src/hev-config.h
+++ src/hev-config.h
@@ -11,8 +11,8 @@
 #define __HEV_CONFIG_H__
 
 #define MAJOR_VERSION (1)
-#define MINOR_VERSION (5)
-#define MICRO_VERSION (9)
+#define MINOR_VERSION (6)
+#define MICRO_VERSION (0)
 
 int hev_config_init (const char *config_path);
 void hev_config_fini (void);
